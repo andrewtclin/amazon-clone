8e8a86bb56fb0c567be9d071245769459dd3f9dc
Andrew <chuntcdj@gmail.com>
2020-09-12 17:58:43 +0800

    Building amazon clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBINDUcZ-iQPHVUP1fVK4Ku0Hl5nIBBl9M",
+  authDomain: "clone-27359.firebaseapp.com",
+  databaseURL: "https://clone-27359.firebaseio.com",
+  projectId: "clone-27359",
+  storageBucket: "clone-27359.appspot.com",
+  messagingSenderId: "210036369012",
+  appId: "1:210036369012:web:297a83f7f97cbec1c69321",
+  measurementId: "G-GLRMBH81CM",
+};
