3ec4a0d30d8c7d2b1963cc89cd7acc2433d104ea
Andrew <chuntcdj@gmail.com>
2020-09-15 17:59:28 +0800

    hiding api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 
 //#1 Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyBINDUcZ-iQPHVUP1fVK4Ku0Hl5nIBBl9M",
-  authDomain: "clone-27359.firebaseapp.com",
-  databaseURL: "https://clone-27359.firebaseio.com",
-  projectId: "clone-27359",
-  storageBucket: "clone-27359.appspot.com",
-  messagingSenderId: "210036369012",
-  appId: "1:210036369012:web:297a83f7f97cbec1c69321",
-  measurementId: "G-GLRMBH81CM",
+  /* Add your own firebase configuration */
 };
 
 //#2 Set up firebase
